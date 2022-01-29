853ba5468ddc2c0156fc0827e7e4e83444a3be6f
Alanis Yates <yatesalanis@gmail.com>
2022-01-28 22:30:26 -0500

    basic maps functionality

--- src/components/pages/Homepage/Homepage.js
+++ src/components/pages/Homepage/Homepage.js
@@ -1,16 +1,20 @@
-import { Box, Typography } from "@mui/material";
+import { Button, Box, Typography, Link } from "@mui/material";
+import { useState } from "react";
+import GooglePlacesAutocomplete from "react-google-places-autocomplete";
 
-
-export const Homepage = () => (
-  <Box
-    sx={{
-      display: "flex",
-      flexDirection: "column",
-      justifyContent: "center",
-      alignItems: "center",
-    }}
-  >
-    <Typography variant="h1" fontWeight="bold">
+export const Homepage = () => {
+  const [value, setValue] = useState(null);
+  const cats = "cats";
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      {/* <Typography variant="h1" fontWeight="bold">
       i-recommend.io
     </Typography>
     <Typography variant="h6" fontWeight="light" pb={6}>
@@ -31,6 +35,32 @@ export const Homepage = () => (
       <Typography variant="h4" fontWeight="medium">
         Coming Soon!
       </Typography>
+    </Box> */}
+      <Typography>Test</Typography>
+      <Box width="300px">
+        <GooglePlacesAutocomplete
+          apiKey="AIzaSyB5gp5idcraT0KrIHQGgqAD5rfgPNfnwAo"
+          selectProps={{
+            value,
+            onChange: setValue,
+          }}
+        />
+      </Box>
+      <Button
+        variant="main"
+        onClick={() => {
+          console.log(value.value.place_id);
+        }}
+      >
+        Hello
+      </Button>
+      <Link
+        // component="button"
+        target="blank"
+        href={`https://www.google.com/maps/search/?api=1&query=Google&query_place_id=${value?.value.place_id}`}
+      >
+        Link to Search
+      </Link>
     </Box>
-  </Box>
-);
+  );
+};
