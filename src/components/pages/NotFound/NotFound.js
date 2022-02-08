cfbac976b0b7601a8e9eb7b0883ff46369cf0efd
Alanis Yates <yatesalanis@gmail.com>
2022-02-07 20:56:34 -0500

    clean up

--- src/components/pages/NotFound/NotFound.js
+++ src/components/pages/NotFound/NotFound.js
@@ -1,9 +1,5 @@
 import { Typography } from "@mui/material";
 
-<<<<<<< HEAD
 export const NotFound = () => (
   <Typography variant="h4">Not Found :( ....</Typography>
 );
-=======
-export const NotFound = () => <Typography variant="h4">Not Found :( </Typography>;
->>>>>>> add-routes
