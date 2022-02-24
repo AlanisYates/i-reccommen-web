b62dda0371673f20abbab5aa2a81d2cf74d020ad
Alanis Yates <yatesalanis@gmail.com>
2022-02-23 21:50:59 -0500

    switch to use the formik lib

--- src/components/pages/Homepage/Homepage.js
+++ src/components/pages/Homepage/Homepage.js
@@ -6,43 +6,73 @@ import {
   ListItem,
   TextField,
   Text,
+  Typography,
+  Modal,
 } from "@mui/material";
 import { useState } from "react";
 import { addPlace } from "../../../_services/PlaceService/PlaceApi";
 import { GoogleSearch } from "./GoogleSearch";
 import { PlaceCard } from "./_components/PlaceCard";
-import { useFormik } from "formik";
+import { Formik, Field, Form, ErrorMessage, FieldArray } from "formik";
 import * as yup from "yup";
+import InputField from "./_components/InputField";
 
-const validationSchema = yup.object({
-  firstName: yup.string("First Name"),
-  lastName: yup.string("Last Name"),
-  email: yup
-    .string("Enter You email")
-    .email("Enter a valid Email")
-    .required("Email is required"),
-});
+
 
 export const Homepage = () => {
   const [recommendationList, setRecommendationList] = useState([]);
   const [value, setValue] = useState(null);
 
-  const formik = useFormik({
-    initialValues: {
-      firstName: "",
-      lastName: "",
-      email: "",
-    },
-    validationSchema: validationSchema,
-    onSubmit: async (values) => {
-      const valuesToSend = {
-        ...values,
-        recomemmendations: recommendationList,
-      };
-      await new Promise((r) => setTimeout(r, 500));
-      await console.log(JSON.stringify(valuesToSend, null, 2));
-    },
+  // Modal--------------->
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  const style = {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: 400,
+    bgcolor: "background.paper",
+    border: "2px solid #000",
+    boxShadow: 24,
+    p: 4,
+  };
+
+  const initialValues = {
+    firstName: "",
+    lastName: "",
+    email: "",
+    recipiants: [],
+  };
+
+  const validationSchema = yup.object({
+    firstName: yup.string("First Name"),
+    lastName: yup.string("Last Name"),
+    email: yup
+      .string("Enter You email")
+      .email("Enter a valid Email")
+      .required("Email is required"),
   });
+
+  // const formik = useFormik({
+  //   initialValues: {
+  //     firstName: "",
+  //     lastName: "",
+  //     email: "",
+  //     recipiants: [],
+  //   },
+  //   validationSchema: validationSchema,
+  //   onSubmit: async (values) => {
+  //     const valuesToSend = {
+  //       ...values,
+  //       recomemmendations: recommendationList,
+  //     };
+  //     await new Promise((r) => setTimeout(r, 500));
+  //     await console.log(JSON.stringify(valuesToSend, null, 2));
+  //   },
+  // });
 
   // const saveItenerary = async () => {
   //   console.log(recommendationList)
@@ -69,60 +99,38 @@ export const Homepage = () => {
 
   return (
     <Box width="80%">
-      <form onSubmit={formik.handleSubmit}>
-        <Box
-          sx={{
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-          <Stack spacing={2} width="100%">
-            <TextField
-              fullWidth
-              variant="standard"
-              id="email"
-              name="email"
-              label="Email"
-              value={formik.values.email}
-              onChange={formik.handleChange}
-              error={formik.touched.email && Boolean(formik.errors.email)}
-              helperText={formik.touched.email && formik.errors.email}
-            />
-            <TextField
-              variant="standard"
-              fullWidth
-              id="lastName"
-              name="lastName"
-              label="Last Name"
-              value={formik.values.lastName}
-              onChange={formik.handleChange}
-              error={formik.touched.lastName && Boolean(formik.errors.lastName)}
-              helperText={formik.touched.lastName && formik.errors.lastName}
-            />
-            <TextField
-              fullWidth
-              variant="standard"
-              id="firstName"
-              name="firstName"
-              label="First Name"
-              value={formik.values.firstName}
-              onChange={formik.handleChange}
-              error={
-                formik.touched.firstName && Boolean(formik.errors.firstName)
-              }
-              helperText={formik.touched.firstName && formik.errors.firstName}
-            />
-            <Box width="100%">
-              <GoogleSearch
-                onClick={(data) => {
-                  setValue(filterData(data));
-                  addRecommendation(filterData(data));
-                }}
-              />
-            </Box>
-            {/* <Stack spacing={4} direction="row" p={4}>
+      <Formik
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+        onSubmit={async (values) => {
+          const dataToSend = {
+            ...values,
+            recomendations: recommendationList
+          }
+          await new Promise((r) => setTimeout(r, 500));
+          alert(JSON.stringify(dataToSend, null, 2));
+        }}
+      >
+        {({ values }) => (
+          <Form>
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <Stack spacing={2} width="100%">
+                <Box width="100%">
+                  <GoogleSearch
+                    onClick={(data) => {
+                      setValue(filterData(data));
+                      addRecommendation(filterData(data));
+                    }}
+                  />
+                </Box>
+                {/* <Stack spacing={4} direction="row" p={4}>
         <Button
           variant="contained"
           color="success"
@@ -134,25 +142,62 @@ export const Homepage = () => {
         </Button>
       </Stack> */}
 
-            <List
-              sx={{
-                overflow: "auto",
-                height: "500px",
-                width: "100%",
-              }}
-            >
-              {recommendationList.map((place) => (
-                <ListItem key={place.name}>
-                  <PlaceCard placeData={place} />
-                </ListItem>
-              ))}
-            </List>
-            <Button type="submit" variant="contained" color="success">
-              Submit
-            </Button>
-          </Stack>
-        </Box>
-      </form>
+                <List
+                  sx={{
+                    overflow: "auto",
+                    height: "500px",
+                    width: "100%",
+                  }}
+                >
+                  {recommendationList.map((place) => (
+                    <ListItem key={place.name}>
+                      <PlaceCard placeData={place} />
+                    </ListItem>
+                  ))}
+                </List>
+                <Button type="submit" variant="contained" color="success">
+                  Submit
+                </Button>
+                <Button onClick={handleOpen}>Open Modal</Button>
+              </Stack>
+
+              <Modal
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+              >
+                <Box sx={style}>
+                  <Typography
+                    id="modal-modal-title"
+                    variant="h6"
+                    component="h2"
+                  >
+                    This is where the Sending and creater info wil go
+                  </Typography>
+                  <InputField name="email" label="Email" />
+                  <InputField name="firstName" label="First Name" />
+                  <InputField name="lastName" label="Last Name" />
+                  <Box
+                    pt={4}
+                    width="100%"
+                    justifyContent="center"
+                    display="flex"
+                  >
+                    <Button
+                      variant="contained"
+                      color="success"
+                      onClick={handleClose}
+                    >
+                      Done
+                    </Button>
+                  </Box>
+                </Box>
+              </Modal>
+            </Box>
+          </Form>
+        )}
+      </Formik>
     </Box>
   );
 };
