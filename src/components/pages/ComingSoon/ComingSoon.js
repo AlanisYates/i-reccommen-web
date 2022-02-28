ecbdc13501490747dde9b39718298d1b5e1defd8
Alanis Yates <ayates@Alaniss-Mac-mini.local>
2022-02-27 20:03:27 -0500

    updated splash page

--- src/components/pages/ComingSoon/ComingSoon.js
+++ src/components/pages/ComingSoon/ComingSoon.js
@@ -1,3 +1,27 @@
+import { Typography } from "@mui/material";
+import { Box } from "@mui/system";
+
 export const ComingSoon = () => {
-  return <>Coming Soon</>;
+  return (
+    <Box
+      display="flex"
+      justifyContent="center"
+      flexDirection="column"
+      alignItems="center"
+    >
+      <Box >
+        <Typography variant="h1">
+          i-recommend.io
+        </Typography>
+      </Box>
+      <Box pb={4}>
+        <Typography variant="h5">
+          The <strong>simple</strong> to recommend places to your friends!
+        </Typography>
+      </Box>
+      <Box bgcolor="white" p={2} sx={{ borderRadius: 30, width: "50%" }}>
+        <Typography variant="h4">Coming Soon!</Typography>
+      </Box>
+    </Box>
+  );
 };
